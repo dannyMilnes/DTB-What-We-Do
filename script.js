bbeb4a3b4efa1b37ad218f04827e67108f55720a
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2021-04-23 16:26:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var content1 = document.getElementById("content1");
+var content2 = document.getElementById("content2");
+var content3 = document.getElementById("content3");
+var content4 = document.getElementById("content4");
+
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+
+function openInsignt() {
+    btn1.style.display="none"
+    btn2.style.display="block"
+    btn3.style.display="block"
+    btn4.style.display="block"
+
+    content1.style.display="block";
+    content2.style.display="none";
+    content3.style.display="none";
+    content4.style.display="none";
+}
+
+function processImprovement() {
+    btn1.style.display = "block"
+    btn2.style.display = "none"
+    btn3.style.display = "block"
+    btn4.style.display = "block"
+
+    content1.style.display="none";
+    content2.style.display="block";
+    content3.style.display="none";
+    content4.style.display="none";
+}
+
+function applicationDevelopment() {
+    btn1.style.display = "block"
+    btn2.style.display = "block"
+    btn3.style.display = "none"
+    btn4.style.display = "block"
+
+    content1.style.display="none";
+    content2.style.display="none";
+    content3.style.display="block";
+    content4.style.display="none";
+}
+
+function cloud() {
+    btn1.style.display = "block"
+    btn2.style.display = "block"
+    btn3.style.display = "block"
+    btn4.style.display = "none"
+
+    content1.style.display="none";
+    content2.style.display="none";
+    content3.style.display="none";
+    content4.style.display="block";
+}
